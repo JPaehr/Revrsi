360430df08a285655c368326ee52687bc7a824b1
Axamr <trash3x@arcor.de>
2012-11-20 14:59:08 +0100

    Tokens werden nun gelöscht und nicht mehr überdeckt. Einstellungen Re…
    ter gelöscht. Feldgröße nun über 'Neues Spiel' einstellbar. Standart Feldgrößen hinzugefügt. 2 8x8,3 9x9,4 10x10. Tokens besitzen nun ihre Position.

--- src/new_game_settings.cpp
+++ src/new_game_settings.cpp
@@ -3,10 +3,14 @@
 
 new_game_settings::new_game_settings(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::new_game_settings)
-{
+    ui(new Ui::new_game_settings){
     ui->setupUi(this);
-    connect(ui->buttonBox,SIGNAL(accepted()),this, SLOT(set_player_number()));
+
+    connect(ui->new_game_settings_buttonbox,SIGNAL(accepted()),this, SLOT(set_settings()));
+    connect(ui->two_player_btn,SIGNAL(clicked()),this,SLOT(change_field_size()));
+    connect(ui->three_player_btn,SIGNAL(clicked()),this,SLOT(change_field_size()));
+    connect(ui->four_player_btn,SIGNAL(clicked()),this,SLOT(change_field_size()));
+
     this->player_number = 2;
     ui->two_player_btn->setChecked(true);
 }
@@ -15,24 +19,53 @@ int new_game_settings::get_choosen_number(){
     return this->player_number;
 }
 
-new_game_settings::~new_game_settings()
-{
+QPoint new_game_settings::get_field_size(){
+    return QPoint(this->x_size, this->y_size);
+}
+
+new_game_settings::~new_game_settings(){
     delete ui;
 }
 
-void new_game_settings::set_player_number()
-{
+void new_game_settings::set_settings(){
     if(ui->two_player_btn->isChecked()){
         this->player_number = 2;
+        this->x_size = ui->breite_spinbox->value();
+        this->y_size = ui->hoehe_spinbox->value();
     }
     else if(ui->three_player_btn->isChecked()){
         this->player_number = 3;
+        this->x_size = ui->breite_spinbox->value();
+        this->y_size = ui->hoehe_spinbox->value();
     }
     else if(ui->four_player_btn->isChecked()){
         this->player_number = 4;
+        this->x_size = ui->breite_spinbox->value();
+        this->y_size = ui->hoehe_spinbox->value();
     }
     else{
         this->player_number = 2;
+        this->x_size = ui->breite_spinbox->value();
+        this->y_size = ui->hoehe_spinbox->value();
     }
     out << "New Game:  Players =" << this->player_number;
+}
+
+void new_game_settings::change_field_size(){
+    if(ui->two_player_btn->isChecked()){
+        ui->breite_spinbox->setValue(8);
+        ui->hoehe_spinbox->setValue(8);
+    }
+    else if(ui->three_player_btn->isChecked()){
+        ui->breite_spinbox->setValue(9);
+        ui->hoehe_spinbox->setValue(9);
+    }
+    else if(ui->four_player_btn->isChecked()){
+        ui->breite_spinbox->setValue(10);
+        ui->hoehe_spinbox->setValue(10);
+    }
+    else{
+        ui->breite_spinbox->setValue(8);
+        ui->hoehe_spinbox->setValue(8);
+    }
 }
