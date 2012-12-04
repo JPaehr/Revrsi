49cf580f2afb5e3fdf84a24f1233349a6322dd85
Axamr <trash3x@arcor.de>
2012-12-05 00:44:06 +0100

    connect funktioniert jetzt zwischen threads

---
+++ QServer/fixedVersion/server.h
@@ -0,0 +1,40 @@
+#ifndef SERVER_H
+#define SERVER_H
+#include <QThread>
+#include "socket.h"
+#include <string>
+#include <vector>
+#include "Logic.h"
+#include <QObject>
+
+using namespace std;
+class Server : public QThread
+{
+    Q_OBJECT
+public:
+    Server(string port, int breite, int hoehe, int anzSpieler, QObject *parent = 0);
+    void run();
+    void senden(string nachrict);
+    vector<vector<int> > getFelder();
+    string StringSpielstand();
+
+    vector<vector<int> > fields;
+
+    void initServer();
+
+private:
+    Logic *ThreadLogic;
+    vector<std::string> explode(const string& str, char delimiter);
+    int width, height, aktPlayer;
+    int breite, hoehe;
+    string port;
+    Socket sock1, sock2;
+    string empfang;
+    string implode( const string &glue, const vector<string> &pieces );
+
+signals:
+    void setStone(int, int, int);
+
+};
+
+#endif // SERVER_H
