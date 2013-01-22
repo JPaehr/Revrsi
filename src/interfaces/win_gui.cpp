a0c2982a2c0b9f962e7010045278bc65c200e6b9
JPaehr <j.paehr@gmx.de>
2013-01-22 22:04:20 +0100

    Berechnung fuer die Plaetze fertig, grafische Umsetzung kommt morgen

--- src/interfaces/win_gui.cpp
+++ src/interfaces/win_gui.cpp
@@ -2,6 +2,9 @@
 #include "ui_win_gui.h"
 #include <QDebug>
 #define out qDebug()
+#include <iostream>
+
+using namespace std;
 
 win_gui::win_gui(QWidget *parent) :
     QDialog(parent),
@@ -18,6 +21,109 @@ win_gui::~win_gui(){
 void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
     win_vector = in_vector;
     player_names = in_names;
+
+    for(int i = 0; i < this->win_vector.size(); i++){
+        cout << "WinVector kommt" << endl;
+        cout << this->win_vector[i] << endl;
+    }
+
+    //anzahl der Plaetze berechnen+
+    vector<int> punkte;
+    for(int i = 1; i < this->win_vector.size(); i++){
+        punkte.push_back(this->win_vector[i]);
+    }
+    int punktegleichstand = 0, anzPlaetze, anzSpieler;
+    for(int i = 0; i < punkte.size(); i++){
+        for(int j = 0; j < punkte.size(); j++){
+            if(punkte[i] == punkte[j] && i != j){
+                punktegleichstand++;
+            }
+        }
+    }
+    anzSpieler = this->win_vector.size()-1;
+    anzPlaetze = anzSpieler-punktegleichstand;
+
+
+    //zuordnung Platz mit punktzahl
+        //hoechstepunktzahl finden
+    int gross = 0, zweitgross = 0, drittgross = 0;
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(gross < this->win_vector[i]){
+            gross = this->win_vector[i];
+        }
+    }
+    vector<vector<int> > platzierung;
+    //alle erste plaetze finden
+
+    vector<int> platzEins, platzZwei, platzDrei, platzvier;
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(this->win_vector[i] == gross){
+            platzEins.push_back(i);
+        }
+    }
+    platzierung.push_back(platzEins);
+
+    //platz 2 finden
+        //zweithoechstepunktzahl finden
+
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(this->win_vector[i] > zweitgross && gross != this->win_vector[i]){
+            zweitgross = this->win_vector[i];
+        }
+    }
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(this->win_vector[i] == zweitgross){
+            platzZwei.push_back(i);
+        }
+    }
+    if(platzZwei.size() > 0){
+        platzierung.push_back(platzZwei);
+    }
+
+    //platz 3 finden
+        //drittgroesste pktzahl finden
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(this->win_vector[i] > drittgross && this->win_vector[i] != gross && this->win_vector[i] != zweitgross){
+            drittgross = this->win_vector[i];
+        }
+    }
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(this->win_vector[i] == drittgross){
+            platzDrei.push_back(i);
+        }
+    }
+    if(platzDrei.size() > 0){
+        platzierung.push_back(platzDrei);
+    }
+
+    //platz vier
+    for(int i = 1; i < this->win_vector.size(); i++){
+        if(this->win_vector[i] != gross && this->win_vector[i] != zweitgross && this->win_vector[i] != drittgross){
+            platzvier.push_back(this->win_vector[i]);
+
+        }
+    }
+    if(platzvier.size() > 0){
+        platzierung.push_back(platzvier);
+    }
+
+    for(int i = 0; i < platzierung.size(); i++){
+        if(i == 0){
+            cout << "Platz 1:" << endl;
+        }
+        if(i == 1){
+            cout << "Platz 2: " << endl;
+        }
+        if(i == 2){
+            cout << "Platz 3:" << endl;
+        }
+        if(i == 3){
+            cout << "Platz 4: " << endl;
+        }
+        for(int j = 0; j < platzierung[i].size(); j++){
+            cout << platzierung[i][j] << endl;
+        }
+    }
 
     /*RückgabeCodes für gleiche Punkzahlen zwischen Spielern:
      *1u2 = -2
