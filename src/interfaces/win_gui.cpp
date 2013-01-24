0f45a62332fa90eb47655c24177e496f1b890591
JPaehr <j.paehr@gmx.de>
2013-01-24 17:04:22 +0100

    win_gui akutalisiert, Fehler nicht ausgeschlossen, szenarien finden in denen 2 spieler die gleiche punktzahl haben, debug-couts aus gleichen Grund noch drin

--- src/interfaces/win_gui.cpp
+++ src/interfaces/win_gui.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #define out qDebug()
 #include <iostream>
+#include "string"
 
 using namespace std;
 
@@ -19,13 +20,19 @@ win_gui::~win_gui(){
 
 
 void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
+    cout << "INVector" << endl;
+    for(int i = 0; i < in_vector.size(); i++){
+        cout << in_vector[i] << endl;
+    }
+    cout << "in names" << endl;
+    std::string name = "";
+    for(int i = 0; i < in_names.size(); i++){
+        name = in_names[i].toStdString();
+        cout << name << endl;
+    }
     win_vector = in_vector;
     player_names = in_names;
 
-    for(int i = 0; i < this->win_vector.size(); i++){
-        cout << "WinVector kommt" << endl;
-        cout << this->win_vector[i] << endl;
-    }
 
     //anzahl der Plaetze berechnen+
     vector<int> punkte;
@@ -46,7 +53,7 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
 
     //zuordnung Platz mit punktzahl
         //hoechstepunktzahl finden
-    int gross = 0, zweitgross = 0, drittgross = 0;
+    int gross = -1, zweitgross = -1, drittgross = -1;
     for(int i = 1; i < this->win_vector.size(); i++){
         if(gross < this->win_vector[i]){
             gross = this->win_vector[i];
@@ -77,6 +84,7 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
         }
     }
     if(platzZwei.size() > 0){
+
         platzierung.push_back(platzZwei);
     }
 
@@ -85,6 +93,8 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
     for(int i = 1; i < this->win_vector.size(); i++){
         if(this->win_vector[i] > drittgross && this->win_vector[i] != gross && this->win_vector[i] != zweitgross){
             drittgross = this->win_vector[i];
+            cout << "drittgross: "<< drittgross << endl;
+            cout << "Das i dazu " << i << endl;
         }
     }
     for(int i = 1; i < this->win_vector.size(); i++){
@@ -93,13 +103,17 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
         }
     }
     if(platzDrei.size() > 0){
+        cout << "platz 3 reingepusht " << endl;
+        for(int i = 0; i <platzDrei.size(); i++){
+            cout << "eintraege " << platzDrei[i] << endl;
+        }
         platzierung.push_back(platzDrei);
     }
 
     //platz vier
     for(int i = 1; i < this->win_vector.size(); i++){
-        if(this->win_vector[i] != gross && this->win_vector[i] != zweitgross && this->win_vector[i] != drittgross){
-            platzvier.push_back(this->win_vector[i]);
+        if((this->win_vector[i] != gross) && (this->win_vector[i] != zweitgross) && (this->win_vector[i] != drittgross)){
+            platzvier.push_back(i);
 
         }
     }
@@ -107,22 +121,115 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
         platzierung.push_back(platzvier);
     }
 
+    //zu versteckende Teile finden
+    int zaehler = 0;
     for(int i = 0; i < platzierung.size(); i++){
-        if(i == 0){
-            cout << "Platz 1:" << endl;
-        }
-        if(i == 1){
-            cout << "Platz 2: " << endl;
-        }
-        if(i == 2){
-            cout << "Platz 3:" << endl;
-        }
-        if(i == 3){
-            cout << "Platz 4: " << endl;
-        }
         for(int j = 0; j < platzierung[i].size(); j++){
-            cout << platzierung[i][j] << endl;
+            zaehler++;
+            cout << "I " << i;
+            cout << "j " << j;
         }
+    }
+    if(zaehler == 2){
+        cout << "Spielercontroll 2" << endl;
+        ui->platz3->hide();
+        ui->platz4->hide();
+        ui->dritter->hide();
+        ui->vierter->hide();
+    }
+    if(zaehler == 3){
+        cout << "Spielercontorll 3" << endl;
+        ui->platz4->hide();
+        ui->vierter->hide();
+    }
+    if(platzierung[0].size() > 1){
+        ui->crown->hide();
+    }
+    int platz = 1;
+    for(int i = 0; i < platzierung.size(); i++){
+        cout << "platz " << platz << endl;
+        if(platz == 4){
+            ui->vierter->setText(in_names[platzierung[i][0]-1]+"\n"+QString::number(in_vector[platzierung[i][0]]) +" Punkte");
+            ui->vierter->move(130, 255);
+            ui->platz4->move(130, 240);
+            platz++;
+        }
+        if(platz == 3){
+            if(platzierung[i].size() > 1){
+                cout << "platz 3 ist doppelt" << endl;
+                ui->dritter->setText(in_names[platzierung[i][0]-1] + "\n"+QString::number(in_vector[platzierung[i][0]]) +" Punkte");
+                ui->dritter->move(195, 175);
+                ui->platz3->move(195, 160);
+
+                ui->vierter->setText(in_names[platzierung[i][1]-1] +"\n"+QString::number(in_vector[platzierung[i][1]]) +" Punkte");
+                ui->vierter->move(260, 175);
+                ui->platz4->setText("2");
+                ui->platz4->move(260, 160);
+                platz+=2;
+            }
+            else{
+                cout << "Platz 3 einzel" << endl;
+                ui->dritter->setText(in_names[platzierung[i][0]-1]+"\n"+QString::number(in_vector[platzierung[i][0]]) +" Punkte");
+                ui->dritter->move(230, 175);
+                ui->platz3->move(230, 160);
+                platz++;
+            }
+        }
+        if(platz == 2){
+
+            cout << "schleife bei platz2 <<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+
+            if(platzierung[1].size() > 1){
+                cout << "platz 2 ist doppelt" << endl;
+                ui->zweiter->setText(in_names[platzierung[i][0]-1] + "\n"+QString::number(in_vector[platzierung[i][0]]) +" Punkte");
+                ui->zweiter->move(65, 175);
+                ui->platz2->move(65, 160);
+
+                ui->dritter->setText(in_names[platzierung[i][1]-1] +"\n"+QString::number(in_vector[platzierung[i][1]]) +" Punkte");
+                ui->dritter->move(200, 175);
+                ui->platz3->setText("2");
+                ui->platz3->move(200, 160);
+                platz+=2;
+            }
+            else{
+                cout << "Platz 2 einzel" << endl;
+                //cout << "winvec " << in_vector[platzierung[1][0]];
+                //cout << platzierung[1][0] << endl;
+                ui->zweiter->setText(in_names[platzierung[i][0]-1] + "\n" + QString::number(in_vector[platzierung[i][0]]) +" Punkte");
+                ui->zweiter->move(65, 175);
+                ui->platz2->move(65, 160);
+                platz++;
+            }
+        }
+        if(platz == 1){
+            if(platzierung[0].size() > 1){
+                cout << "platz 1 ist doppelt" << endl;
+                ui->erster->move(65, 105);
+                ui->erster->setText(in_names[platzierung[i][0]-1] +"\n"+ QString::number(in_vector[platzierung[i][0]])+" Punkte");
+                ui->platz1->move(65, 90);
+                ui->crown->hide();
+
+                ui->zweiter->move(230, 105);
+                ui->zweiter->setText(in_names[platzierung[i][1]-1] + "\n" + QString::number(in_vector[platzierung[i][1]])+" Punkte");
+                ui->platz2->setText("1");
+                ui->platz2->move(230, 90);
+                platz+=2;
+            }
+            else{
+                //cout << "winvec" << in_vector[platzierung[0][0]] << endl;
+
+                ui->erster->setText(in_names[platzierung[i][0]-1]+"\n"+QString::number(in_vector[platzierung[i][0]])+ " Punkte");
+                //ui->platz1pkt->setText(QString::number(in_vector[platzierung[0][0]])+ " Punkte");
+                //ui->platz1pkt->move(130, 80);
+                ui->erster->move(130, 105);
+                ui->platz1->move(130, 90);
+                platz+=1;
+                cout << "schleife1 hat einen nach oben gezaehlt" << endl;
+            }
+        }
+
+
+
     }
 
     /*RückgabeCodes für gleiche Punkzahlen zwischen Spielern:
@@ -138,7 +245,7 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
      *2u3u4 = -11
      *1u2u3u4 = -12
     */
-
+    /*
     if(win_vector.size() == 3){
         ui->platz3->hide();
         ui->dritter->hide();
@@ -581,4 +688,5 @@ void win_gui::win_slot(QVector<int> in_vector, QVector<QString> in_names){
             ui->crown->setVisible(true);
         }
     }
+    */
 }
